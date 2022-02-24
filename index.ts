4dd3d27ef5807c03ec5915b0742167551470354d
titusdishon <titusdishon@gmail.com>
2022-02-24 22:12:01 +0300

    change port number

--- index.ts
+++ index.ts
@@ -7,11 +7,12 @@ import { Server } from "socket.io";
 import http from "http";
 import express from "express";
 const app = express();
-const port = 3002;
+const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://cocky-agnesi-f3e7c4.netlify.app/",
+    methods: ["GET", "POST"]
   },
 });
 let messages = [];
